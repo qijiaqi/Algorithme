d62649c291406a612a086687fbc24a01fbbce4ba
qijiaqi <jiaqi.qi@insa-lyon.fr>
2023-04-06 10:55:37 +0200

    309. Best Time to Buy  and Sell Stock with Cooldown

---
+++ LC309_bestTimeStockCooldown.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+#include<algorithm>
+
+using namespace std;
+
+int main () {
+    vector<int> n;
+    int size;
+    cin >> size;
+    int a;
+    for(int i = 0; i < size; ++ i) {
+        cin >> a;
+        n.push_back(a);
+    }
+
+    // DP
+    vector<vector<int>> dp(size, vector<int>(4, 0));
+    dp[0][0] = -n[0];
+    for (int i = 1; i < size; ++ i) {
+        dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][1], dp[i - 1][3]) - n[i]); // buy
+        dp[i][1] = max(dp[i - 1][1], dp[i - 1][3]); // renew
+        dp[i][2] = dp[i - 1][0] + n[i]; // sell with cooldown
+        dp[i][3] = dp[i - 1][2]; // free
+    }
+
+    cout << max(dp[size - 1][1], max(dp[size - 1][2], dp[size - 1][3])) << endl;
+    return 0;
+    }
