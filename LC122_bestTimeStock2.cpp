598cfa3a1618a9e10786c6f19a980cb546487b9e
qijiaqi <jiaqi.qi@insa-lyon.fr>
2023-04-11 00:09:35 +0200

    122. Best Time to Buy and Sell Stock 2

--- LC122_bestTimeStock2.cpp
+++ LC122_bestTimeStock2.cpp
@@ -17,16 +17,16 @@ int main () {
     }
 
     // DP
-    vector<vector<int>> dp(size, vector<int>(2, 0)); 
+    vector<vector<int>> dp(size, vector<int>(2, 0)); // dp[i][j]: max profit on day i in state j
     dp[0][1] = -n[0]; // 1: stock in hand
     for (int i = 1; i < size; ++ i) { 
         dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - n[i]); // can buy several times
-        dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + n[i]);
+        dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + n[i]); // sell
     }
     // GR
     int sum = 0, min = n[0];
     for (int i = 1; i < size; ++ i) {
-        if (min < n[i]) sum += n[i] - min;
+        if (min < n[i]) sum += n[i] - min; // if profit is positive, we sell
         min = n[i];
     }
 
