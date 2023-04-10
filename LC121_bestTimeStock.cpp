991f275ffcd86f90dbe5cf4d9c3e6b53c165b3d4
qijiaqi <jiaqi.qi@insa-lyon.fr>
2023-04-11 00:09:17 +0200

    121. Best Time to Buy and Sell Stock 1

--- LC121_bestTimeStock.cpp
+++ LC121_bestTimeStock.cpp
@@ -17,11 +17,11 @@ int main () {
     }
 
     // DP
-    vector<vector<int>> dp(size, vector<int>(2, 0)); 
+    vector<vector<int>> dp(size, vector<int>(2, 0)); // dp[i][j]: max profit on day i in state j 
     dp[0][1] = -n[0]; // 1: stock in hand
     for (int i = 1; i < size; ++ i) { 
         dp[i][1] = max(dp[i - 1][1], -n[i]); // can buy just one time
-        dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + n[i]);
+        dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + n[i]); // sell
     }
 
     cout << dp[size - 1][0] << endl;
