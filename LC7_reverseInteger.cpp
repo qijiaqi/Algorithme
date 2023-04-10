a5c7c9dbf7561c83784e62a809a2ce6010df08a7
qijiaqi <jiaqi.qi@insa-lyon.fr>
2023-04-11 00:07:17 +0200

    7. Reverse Integer

--- LC7_reverseInteger.cpp
+++ LC7_reverseInteger.cpp
@@ -6,22 +6,31 @@
 using namespace std;
 
 int main () {
-    int x;
+    int x, c;
     cin >> x;
+    c = x;
 
-    int y = 0, z = 1; // long y, (int)y
-        if(x < 0) {
-            z = -1;
-            if(x > INT_MIN) x *= -1; // x = INT_MIN
-            else return 0;
-        }
-        while(x != 0) {
-            int tmp = x%10;
-            if( 214748364 > y || (y == 214748364 && tmp < 7) ) y = y*10 + tmp; // (INT_MAX - tmp)/10
-            else return 0;
+    int y = 0;
+    if (x > INT_MIN) { // INT_MAX = 2147483647, INT_MIN = -2147483648
+        while (x != 0) {
+            int z = x%10;
+            if ( 214748364 > abs(y) || (abs(y) == 214748364 && z < 7) ) y = y*10 + z; 
+            // round problem: (INT_MAX - z)/10
+            else {
+                y = 0;
+                break;
+            }
             x /= 10;
         }
+    }
+    // conversion: long y == (int)y
+    int a = 0;
+    while (c != 0) {
+        a = a*10 + c%10;
+        c /= 10;
+    }
+    int b = (a == (int)a) ? a : 0; // 0 if overflow
 
-    cout << y*z;
+    cout << y << " " << b << endl;
     return 0;
 }
