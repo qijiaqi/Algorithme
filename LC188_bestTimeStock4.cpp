5251567dc0f20a1e5acfec3aa284f4e860a6c262
qijiaqi <jiaqi.qi@insa-lyon.fr>
2023-04-11 00:10:24 +0200

    188. Best Time to Buy and Sell Stock 4

--- LC188_bestTimeStock4.cpp
+++ LC188_bestTimeStock4.cpp
@@ -17,7 +17,7 @@ int main () {
     }
 
     // DP
-    vector<vector<int>> dp(size, vector<int>(2*k, 0)); 
+    vector<vector<int>> dp(size, vector<int>(2*k, 0)); // dp[i][j]: max profit on day i in state j 
     for (int i = 0; i < 2*k; i += 2) dp[0][i] = -n[0]; 
     for (int i = 1; i < size; ++ i) { 
         dp[i][0] = max(dp[i - 1][0], -n[i]); // buy
