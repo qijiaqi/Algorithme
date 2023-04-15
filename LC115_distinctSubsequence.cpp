8504225e275ec9f47a4258e83ce43f5bd96bc5c5
qijiaqi <jiaqi.qi@insa-lyon.fr>
2023-04-16 00:33:12 +0200

    115. Distinct Subsequences

--- LC115_distinctSubsequence.cpp
+++ LC115_distinctSubsequence.cpp
@@ -17,9 +17,13 @@ int main () {
     for (int i = 1; i <= t.size(); ++ i) {
         for (int j = 1; j <= s.size(); ++ j) {
             if (s[j - 1] == t[i - 1] && dp[i - 1][j - 1] < INT_MAX - dp[i][j - 1]) { // overflow
-                dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]; // (ba, babgba) = (ba, babgb) + (b, babgba)
+                dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]; 
+                // (ba, babgba) = (ba, babgb) + (b, babgb) = 1 + 3
+                // existing quantity + generated quantity
             } else {
-                dp[i][j] = dp[i][j - 1]; // (b, ba) = (b, b) where (i, j) correponds (t, s)
+                dp[i][j] = dp[i][j - 1]; 
+                // (ba, babgb) = (ba, babg) = 2 
+                // where (i, j) correponds (t, s)
             }
         }
     }
