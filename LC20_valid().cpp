edf1298f32260f0c060e9f0b2f481dc594c4864b
qijiaqi <jiaqi.qi@insa-lyon.fr>
2023-07-16 16:36:09 +0200

    20. Valid Parentheses

---
+++ LC20_valid().cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+#include<algorithm>
+
+using namespace std;
+
+int main () {
+    string s;
+    cin >> s;
+
+    // Stack
+    stack<char> st;
+    for (char c : s) {
+        if (c == '[') st.push(']');
+        else if (c == '(') st.push(')');
+        else if (c == '{') st.push('}');
+        else if (st.empty() || st.top() != c) {
+            cout << false << endl;
+            return 0;
+        } else st.pop();
+    }
+
+    cout << st.empty() << endl;
+    reutrn 0;
+}
