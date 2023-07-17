927b787606adfa91c3215480ba08d0a3e84ad0c8
qijiaqi <jiaqi.qi@insa-lyon.fr>
2023-07-17 21:00:54 +0200

    190. Reverse Bits

---
+++ LC190_reverseBit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+#include<algorithm>
+
+using namespace std;
+
+int main () {
+    unit32_t n;
+    cin >> n;
+
+    // Bit
+    unit32_t m = 0;
+    for (int i = 1; i <= 32; ++ i) {
+        m |= (n & 1) << (32 - i);
+        n >>= 1; 
+    }
+
+    cout << m << endl;
+    return 0;
+}
