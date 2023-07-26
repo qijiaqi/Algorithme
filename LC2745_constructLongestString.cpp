b8ffa94bac228607f586c485733dcd3a2d957ee1
qijiaqi <jiaqi.qi@insa-lyon.fr>
2023-07-26 22:44:14 +0200

    2745. Construct the Longest New String

--- LC2745_constructLongestString.cpp
+++ LC2745_constructLongestString.cpp
@@ -20,6 +20,23 @@ int dfs (int x, int y, int z, int pre) {
     return max(dfs(x, y, z-1, 3), dfs(x-1, y, z, 1)) + 2;
 }
 
+int m[51][51][51][4];
+int ms (int x, int y, int z, int pre) {
+    if (m[x][y][z][pre] != -1) return m[x][y][z][pre];
+    if (pre == 1) {
+    // AA
+        if (y == 0) m[x][y][z][pre] = 0;
+        else m[x][y][z][pre] = ms(x, y-1, z, 2) + 2;
+    } else {
+    // BB || AB
+        if (x == 0 && z == 0) m[x][y][z][pre] = 0;
+        else if (x == 0) m[x][y][z][pre] = ms(x, y, z-1, 3) + 2;
+        else if (z == 0) m[x][y][z][pre] = ms(x-1, y, z, 1) + 2;
+        else m[x][y][z][pre] = max(ms(x, y, z-1, 3), ms(x-1, y, z, 1)) + 2;
+    }
+    return m[x][y][z][pre];
+}
+
 int main () {
     int x, y, z;
     cin >> x >> y >> z;
@@ -28,5 +45,8 @@ int main () {
     cout << 2*(2*min(x, y) + z + (x!=y)) << endl;
     // DFS : AB+ impossible && Time Out!
     cout << max(dfs(x, y, z, 1), dfs(x, y, z, 2)) << endl;
-    // MS :
+    // MS : memory research
+    memset(m, -1, sizeof(m));
+    cout << max(ms(x, y, z, 1), ms(x, y, z, 2)) << endl;
+    
 }
